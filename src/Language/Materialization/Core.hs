bb7bd86221f6eea112190f0008b5be030809100c
P.C. Shyamshankar <shyam@cs.jhu.edu>
2017-03-07 11:52:22 -0500

    Revamp AST to include Return, etc.

--- src/Language/Materialization/Core.hs
+++ src/Language/Materialization/Core.hs
@@ -1,85 +1,95 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Language.Materialization.Core where
 
 import Text.PrettyPrint.Annotated
 import Language.Common.Pretty (Pretty(..))
 
--- | Programs are blocks with designated return locations.
-data Program = Program { programBody :: Block, programReturn :: Location }
- deriving (Eq, Ord, Read, Show)
+-- | Programs are sequences of clauses, parameterized by a name type.
+type Program = [Clause]
 
 instance Pretty Program where
-  pretty (Program {..}) = lbrace $+$ nest 2 (pretty programBody) $+$ rbrace <+> "~>" <+> pretty programReturn
+  pretty b = vcat (map pretty b)
 
--- | Blocks are sequences of clauses.
-newtype Block = Block { blockBody :: [Clause] }
- deriving (Eq, Ord, Read, Show)
-
-instance Pretty Block where
-  pretty (Block b) = vcat (map pretty b)
-
+-- | Clauses are purely side-effecting statements, typically targeting a single name.
 data Clause
-  -- | Introduction of a new location into the environment.
-  = Allocate Location
+  -- | Initialize a name from a value expression.
+  = Initialization Name ValueExpression
 
-  -- | Initialization of a location with a (static) value expression.
-  | Initialize Location ValueExpression
+  -- | Materialize a name from data stored under another, either directly, or through function
+  -- application.
+  | Materialization Name NameExpression Method
 
-  -- | Materialization of a value in one location into another, according to a designated
-  -- materialization method.
-  | Materialize Location LocationExpression Method
+  -- | Synchronize the contents of a name.
+  | Synchronization Name
 
-  -- | Deallocation of an existing location from the environment.
-  | Deallocate Location
+  -- | Return from a function application, performing all necessary clean-up activities.
+  | Return
  deriving (Eq, Ord, Read, Show)
 
 instance Pretty Clause where
   pretty c = case c of
-    Allocate loc -> "+" <> pretty loc <> semi
-    Initialize loc vexpr -> pretty loc <+> "=" <+> pretty vexpr <> semi
-    Materialize loc lexpr m -> pretty loc <+> "<" <> pretty m <> "-" <+> pretty lexpr <> semi
-    Deallocate loc -> "-" <> pretty loc <> semi
+    Initialization name vexpr -> pretty name <+> "=" <+> pretty vexpr
+    Materialization name lexpr m -> pretty name <+> "<" <> pretty m <> "-" <+> pretty lexpr
+    Synchronization name -> pretty name <> "?"
+    Return -> "!"
 
-instance Pretty LocationExpression where
-  pretty lexpr = case lexpr of
-    LPrimitive loc -> pretty loc
-    Application floc xloc m -> pretty floc <+> parens (pretty m) <+> pretty xloc
+-- | Expressions resulting in names.
+data NameExpression
+  -- | Just a plain name.
+  = PrimitiveName Name
+
+  -- | An application of a closure stored under one name to an argument stored under another,
+  -- materializing the formal parameter by the designated materialization method.
+  | Application Name Name Method
+ deriving (Eq, Ord, Read, Show)
+
+instance Pretty NameExpression where
+  pretty nexpr = case nexpr of
+    PrimitiveName name -> pretty name
+    Application fName xName m -> pretty fName <+> parens (pretty m) <+> pretty xName
 
 -- | Expressions resulting in values.
 data ValueExpression
   -- | Just a plain value.
-  = VPrimitive Value
+  = AtomValue
+  | BlobValue
 
-  -- | A function abstraction, with a formal parameter, closure specification and body.
-  | Abstraction Location CaptureSpec Program
- deriving (Eq, Ord, Read, Show)
-
--- | Expressions resulting in locations.
-data LocationExpression
-  -- | Just a plain location.
-  = LPrimitive Location
-
-  -- | An application of a closure stored in one location to an argument stored in another location,
-  -- materializing the formal parameter by the designated materialization method.
-  | Application Location Location Method
+  -- | A capture expression; a data constructor with a designated capture specification.
+  | CapturingVExpr CaptureExpression
  deriving (Eq, Ord, Read, Show)
 
 instance Pretty ValueExpression where
   pretty vexpr = case vexpr of
-    VPrimitive v -> pretty v
-    Abstraction xloc cspec body -> "\\" <> pretty xloc <+> pretty cspec $+$ pretty body
+    AtomValue -> "a"
+    BlobValue -> "b"
+    CapturingVExpr c -> pretty c
 
--- | Specification of how closure locations are captured --- the materialization method, and the
--- inner and outer locations.
-newtype CaptureSpec = CaptureSpec [(Location, Location, Method)]
+-- | Expressions which make use of captured names; typically data constructors such as functions.
+data CaptureExpression
+  -- | Function abstractions, with a formal parameter, body and return parameter.
+  = CaptureExpression CaptureSpec DataConstructor
+ deriving (Eq, Ord, Read, Show)
+
+instance Pretty CaptureExpression where
+  pretty (CaptureExpression cSpec dCon) = "\\" <> pretty cSpec <> "." $+$ pretty dCon
+
+-- | Specification of how names are captured -- their names on the inside and the outside, as well
+-- as the materialization method used.
+newtype CaptureSpec = CaptureSpec [(Name, Name, Method)]
  deriving (Eq, Ord, Read, Show)
 
 instance Pretty CaptureSpec where
-  pretty (CaptureSpec cspec) = brackets $ hsep $ punctuate comma $ map prettyCapture cspec
-   where
-    prettyCapture (iLoc, oLoc, m) = pretty iLoc <+> "<" <> pretty m <> "-" <+> pretty oLoc
+  pretty (CaptureSpec cSpec) = brackets $ hsep $ punctuate comma $ map prettyCapture cSpec
+   where prettyCapture (iName, oName, m) = pretty iName <+> "<" <> pretty m <> "-" <+> pretty oName
+
+data DataConstructor = Abstraction Name Program Name
+ deriving (Eq, Ord, Read, Show)
+
+instance Pretty DataConstructor where
+  pretty (Abstraction xName pBody rName) = "\\" <> pretty xName <> "." $+$ pretty pBody <+> "~>" <+> pretty rName
 
 -- | Methods of materialization.
 data Method = Copy | Move | Refr
@@ -91,26 +101,27 @@ instance Pretty Method where
     Move -> "M"
     Refr -> "R"
 
--- | Named locations, accessible from the program.
-newtype Location = Location String
+-- | Program-level names.
+newtype Name = Name String
  deriving (Eq, Ord, Read, Show)
 
-instance Pretty Location where
-  pretty (Location loc) = text loc
+instance Pretty Name where
+  pretty (Name n) = text n
 
--- | Values, stored in locations.
-data Value
-  = Atom
-  | Closure Location ClosureSpec Program
+-- | Types serve to distinguish between small values, big values and closure values.
+data Type = AtomType | BlobType | ClosureType Type Type CaptureType
  deriving (Eq, Ord, Read, Show)
 
-instance Pretty Value where
-  pretty v = case v of
-    Atom -> "atom"
-    Closure xloc clspec body -> "\\" <> pretty xloc <+> pretty clspec $+$ pretty body
+instance Pretty Type where
+  pretty t = case t of
+    AtomType -> "A"
+    BlobType -> "B"
+    ClosureType ta tb tc -> pretty ta <+> "-" <> pretty tc <> ">" <+> pretty tb
 
-newtype ClosureSpec = ClosureSpec [Location]
- deriving (Eq, Ord, Read, Show)
+-- | The type of a capture is the set of associations between captured names (on the inside of a
+-- captured value) and their corresponding types.
+type CaptureType = [(Name, Type)]
 
-instance Pretty ClosureSpec where
-  pretty (ClosureSpec ls) = brackets (hsep $ punctuate comma $ map pretty ls)
+instance Pretty CaptureType where
+  pretty nts = brackets $ hsep $ punctuate comma $ map prettyCaptureType nts
+   where prettyCaptureType (n, t) = pretty n <> colon <+> pretty t
