aff55bcc0a2a99e2bc2be6e841727d7463a64387
P.C. Shyamshankar <shyam@cs.jhu.edu>
2017-04-08 18:28:57 -0400

    Add block header pretty printing helpers.

--- src/Language/Common/Pretty.hs
+++ src/Language/Common/Pretty.hs
@@ -1,4 +1,10 @@
-module Language.Common.Pretty (Pretty(..), MDoc, module Text.PrettyPrint.Annotated) where
+module Language.Common.Pretty (
+  Pretty(..),
+  MDoc,
+  module Text.PrettyPrint.Annotated,
+
+  block
+) where
 
 import Text.PrettyPrint.Annotated
 
@@ -7,3 +13,6 @@ type MDoc = Doc MAnn
 
 class Pretty a where
   pretty :: a -> MDoc
+
+block :: Char -> String -> MDoc -> MDoc
+block c t d = text (replicate 3 c) <+> text t <+> text (replicate 3 c) $+$ d
