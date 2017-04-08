8514f9cb469f16169d17614fdde884cafb6ac98b
P.C. Shyamshankar <shyam@cs.jhu.edu>
2017-04-08 18:29:51 -0400

    Get CLI to interpret programs.

--- cli/systemM.hs
+++ cli/systemM.hs
@@ -5,13 +5,16 @@ import Options.Applicative
 
 import Language.Common.Pretty (pretty, render)
 import Language.Materialization.Parser
+import Language.Materialization.Interpreter
 
-data Options = Options
+newtype Options = Options
   { sourcePath :: String
   }
 
 optionsParser :: Parser Options
-optionsParser = Options <$> argument str (metavar "PATH")
+optionsParser = do
+  src <- argument str (metavar "PATH")
+  pure Options { sourcePath = src }
 
 main :: IO ()
 main = do
@@ -19,7 +22,9 @@ main = do
   source <- readFile (sourcePath options)
   case runProgramParser source of
     Left e -> putStrLn $ parseErrorPretty e
-    Right r -> putStrLn $ render $ pretty r
+    Right r -> do
+      let result = runInterpretation (cfgToEnd $ r :/: nil) mempty
+      putStrLn $ render $ pretty result
  where
   programOptionsParser = info (optionsParser <**> helper)
     ( fullDesc <> header "systemM -- A Materialization Calculus"
