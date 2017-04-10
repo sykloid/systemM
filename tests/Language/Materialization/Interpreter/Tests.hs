fa3b6637efec3cb7ded41abc891c87ec9a9d2d37
P.C. Shyamshankar <shyam@cs.jhu.edu>
2017-04-10 12:06:26 -0400

    Update interpreter tests.

--- tests/Language/Materialization/Interpreter/Tests.hs
+++ tests/Language/Materialization/Interpreter/Tests.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 module Language.Materialization.Interpreter.Tests (tests) where
 
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import Language.Common.Pretty
 import Language.Materialization.Core
 import Language.Materialization.Interpreter
 import Language.Materialization.Quoter
@@ -11,17 +13,60 @@ import Language.Materialization.Quoter
 tests :: TestTree
 tests = testGroup "Interpreter"
   [ testCase "Synchronization of Non-Resolving Names" case_syncNonResolving
+  , testCase "Initialization of Small Values" case_initializeSmallValue
+  , testCase "Initialization of Large Values" case_initializeLargeValue
+  , testCase "Initialization of Function Values" case_initializeFunctionValue
+  , testCase "Capture of Small Values" case_smallCapture
+  , testCase "Capture of Multiple Values" case_multiCapture
   ]
 
-(@!?) :: Program -> (InterpretationResult Store -> Assertion) -> Assertion
-(@!?) p f = case runInterpretation (cfgToEnd (p :/: nil)) mempty of
+(@!?) :: Configuration -> (InterpretationResult Store -> Assertion) -> Assertion
+(@!?) p f = case runInterpretation (cfgToEnd p) mempty of
   ((Left err, _), _) -> assertFailure $ show err
   result -> f result
 
-(@!!*) :: Program -> InterpretationError -> Assertion
-(@!!*) p f = case runInterpretation (cfgToEnd (p :/: nil)) mempty of
+(@!!*) :: Configuration -> InterpretationError -> Assertion
+(@!!*) p f = case runInterpretation (cfgToEnd p) mempty of
   ((Right _, _), _) -> assertFailure "It actually succeeded."
   ((Left e, _), _) -> e @?= f
 
+(@!=?) :: LeftExpression -> ShallowValue -> (Store -> Interpretation Assertion)
+(@!=?) lExpr sVal s = do
+  lIdentAddr <- fromShare <$> resolve lExpr s !? AllocationError lExpr
+  lValue <- inspect lIdentAddr s
+  return $ lValue @=? sVal
+
+infix 2 @!?
+infix 2 @!!*
+
+-- Run a configuration, get a store, run a query on the store to get an assertion, and assert it.
+(~=>) :: Configuration -> (Store -> Interpretation Assertion) -> Assertion
+(~=>) c f = case runInterpretation (cfgToEnd c >>= f) mempty of
+  ((Right a, _), _) -> a
+  ((Left e, _), _) -> assertFailure (render $ pretty e)
+
+infix 1 ~=>
+
 case_syncNonResolving :: Assertion
-case_syncNonResolving = [mP|x ?|]  @!!* ResolutionError (Unqualified (Name "x"))
+case_syncNonResolving = [mP|x?|] :/: nil @!!* NameResolutionError (Unqualified (Name "x"))
+
+case_initializeSmallValue :: Assertion
+case_initializeSmallValue = ([mP|x = small-1|] :/: nil) ~=> [mL|x|] @!=? SmallValue "1"
+
+case_initializeLargeValue :: Assertion
+case_initializeLargeValue = ([mP|x = large-1|] :/: nil) ~=> [mL|x|] @!=? LargeValue "1"
+
+case_initializeFunctionValue :: Assertion
+case_initializeFunctionValue = ([mP|x = \[]. \y. {} -> (y? * C)|] :/: nil) ~=>
+  [mL|x|] @!=? (FunctionValue $ Abstraction "y" []
+                (BidExpression $ Bid (Synchronizing $ Unqualified $ Name "y") Copy))
+
+case_smallCapture :: Assertion
+case_smallCapture = [mP| q = small-1;
+                         x = \[z = (q? * C)]. \y. {} -> \[]. \w. {} -> (z? * C)
+                        |] :/: nil ~=> [mL|x.z|] @!=? SmallValue "1"
+
+case_multiCapture :: Assertion
+case_multiCapture = [mP| q1 = small-1; q2 = large-1;
+                         f = \[w1 = (q1? * C), w2 = (q2? * C)]. \y. {} -> (w2 * C)
+                       |] :/: nil ~=> [mL|f.w2|] @!=? LargeValue "1"
