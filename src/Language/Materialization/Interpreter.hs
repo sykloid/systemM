199f05082429ae34a725218e18ba04d331f8585d
P.C. Shyamshankar <shyam@cs.jhu.edu>
2017-04-17 12:55:37 -0400

    Minor formatting.

--- src/Language/Materialization/Interpreter.hs
+++ src/Language/Materialization/Interpreter.hs
@@ -577,7 +577,10 @@ stepOnce ((c:cs) :/: s) = case c of
                      ] ++ body ++
                      [ Assignment lExpr returnExpr
                      , Return
-                     ] ++ cs :/: (s { environment = (environment s) { stack = nil { closure = fDeps }: stack (environment s) }})
+                     ] ++ cs :/: (s { environment = (environment s)
+                                        { stack = nil { closure = fDeps }: stack (environment s) }
+                                    }
+                                 )
 
           -- This would be more disciplined as a type error.
           _ -> throwE "Application of a non-function."
